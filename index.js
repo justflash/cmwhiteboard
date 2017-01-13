bf7dc76eea4d28cf807029a85b2b457b4727abdd
sts <sathesh.fdd@gmail.com>
2017-01-13 10:10:51 +0530

    update

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-import 'assets/js/whiteboard_v3.js';
+var wb=require('/assets/js/whiteboard_v3.js');
 function cmwbPlugin(){
 	console.log("cmwb plugin init here!");
+console.log(wb);
 	console.log(Whiteboard);
+
 }
 module.exports={
 create:function(){
